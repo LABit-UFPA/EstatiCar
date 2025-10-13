ebe38303b86f0d014d642eae0cf8d2ed550f9841
kimlimalima <kimlimaufpa@gmail.com>
2025-10-13 14:39:49 -0300

    atualizando credentials

--- src/app/Controller/load_credentials.py
+++ src/app/Controller/load_credentials.py
@@ -3,7 +3,7 @@ from Controller.load_path import load_path
 
 def load_credentials():
     config_path_database = load_path("json/databasepath.json")  
-    config_path_sqlite_database = load_path("DB/app_data_base.db")
+    config_path_sqlite_database = load_path("db/app_data_base.db")
 
     with open(config_path_database, 'r') as config_file_database:
         config_data_path_database = json.load(config_file_database)
