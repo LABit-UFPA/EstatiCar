75c22b350ca433b0adfbf39d4407539f1902ca01
kimlimalima <kimlimaufpa@gmail.com>
2025-10-15 23:19:57 -0300

    correção do assets

--- src/app/Components/footer_components.py
+++ src/app/Components/footer_components.py
@@ -3,10 +3,10 @@ import flet as ft
 class Footer:
 
     footer_images = [
-            ft.Image(src="src/app/Assets/images/capes_logo-removebg-preview.png", width=70, height=70, fit="contain"),
-            ft.Image(src="src/app/Assets/images/ufpa_logo.png", width=70, height=70, fit="contain"),
-            ft.Image(src="src/app/Assets/images/ppgsp_logo.png", width=70, height=70, fit="contain"),
-            ft.Image(src="src/app/Assets/images/lab_logo.png", width=70, height=70, fit="contain"),
-            ft.Image(src="src/app/Assets/images/pcpa_logo.png", width=70, height=70, fit="contain"),
-            ft.Image(src="src/app/Assets/images/logo-siac.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/capes_logo-removebg-preview.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/ufpa_logo.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/ppgsp_logo.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/lab_logo.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/pcpa_logo.png", width=70, height=70, fit="contain"),
+            ft.Image(src="Assets/images/logo-siac.png", width=70, height=70, fit="contain"),
         ]
